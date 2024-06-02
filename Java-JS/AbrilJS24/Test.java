5c7af747f568c66b880829b152fa6e4fee47649a
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-02 15:10:41 -0500

    Create Test.java

---
+++ Java-JS/AbrilJS24/Test.java
@@ -0,0 +1,13 @@
+package Java-JS.AbrilJS24;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        /* Variables primitivas */
+        int edad = 28; // Int -> númerico entero
+        double estatura = 1.85; // double -> númerico decimal
+        char genero = 'M'; // char -> caracter sencillo
+        boolean casado = true; // boolean -> valor lógico true/false
+    }
+}
