90b931e8c0f16c9bb67297da6f319da5b1686984
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-02 14:55:53 -0500

    New Task

---
+++ Java-JS/AbrilJS-23/FirstProgJava/Task.java
@@ -0,0 +1,10 @@
+package FirstProgJava;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Coding Dojo");
+        System.out.println("Tengo 100 años de edad");
+        System.out.println("Mi ciudad natal es Burnak, CA");
+    }
+    
+}
