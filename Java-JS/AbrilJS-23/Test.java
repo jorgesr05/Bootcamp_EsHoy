ead8d7e34716e134ac895593e4612fc9358099f6
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-02 10:52:02 -0500

    su

--- Java-JS/AbrilJS-23/Test.java
+++ Java-JS/AbrilJS-23/Test.java
@@ -1,5 +1,8 @@
 
 public class Test {
-
+ /* Función principal debe ser única en todo el proyecto */
+        public static void main(String[] args) {
+            System.out.println("Hola a todos");
+        }
     
 }
