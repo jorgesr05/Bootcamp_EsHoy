fb8f850b10916940b052c425809c69f965a96ab8
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-02 22:44:25 -0500

    Update VariablesTest.java

--- Java_JS/AbrilJS25/VariablesTest.java
+++ Java_JS/AbrilJS25/VariablesTest.java
@@ -1,5 +1,9 @@
 package Java_JS.AbrilJS25;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 public class VariablesTest {
     
     public static void main(String[] args) {
@@ -16,8 +20,8 @@ public class VariablesTest {
 
         
         long start = System.currentTimeMillis();
-        Integer sum = 0;
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
             sum += i;
         }
         System.out.println("Suma: " + sum);
@@ -25,8 +29,79 @@ public class VariablesTest {
         double total = (double) (end - start) / 1000;
         System.out.println("Tiempo de ejecución: " + total + " segundos");
         
+        long start2 = System.currentTimeMillis();
+        int sum2 = 0;
+        for (byte i = 0; i < 100; i++) {
+            sum2 += i;
+        }
+        System.out.println("Suma: " + sum2);
+        long end2 = System.currentTimeMillis();
+        double total2 = (double) (end2 - start2) / 1000;
+        System.out.println("Tiempo de ejecución: " + total2 + " segundos");
+
+        int[] bingo;
+        bingo = new int[5];
+        bingo[0] = 12;
+        bingo[1] = 16;
+        bingo[2] = 32;
+        bingo[3] = 18;
+        bingo[4] = 7;
+
+        System.out.println(bingo[3]);
+
+        int[] loteria = {4, 8, 48, 5, 9};
+        System.out.println(loteria[2]);
+
+        ArrayList<Integer> chance = new ArrayList<Integer>();
+        chance.add(42);
+        chance.add(38);
+        System.out.println(chance);
+        chance.add(18);
+        System.out.println(chance);
+
+        int num = (int) (Math.random()*10);
+
+        while (num<7) {
+            num = (int) (Math.random()*10);
+            System.out.println(num);
+            
+        }
+        System.out.println("-------------------------");
+        for(int i=0; i<20; i+=2){
+            System.out.println(i);
+        }
+
+        System.out.println("-------------------------");
+        for(int j=0; j<loteria.length; j+=2){
+            System.out.println(loteria[j]);
+        }
+
+        System.out.println(chance.getLast());
+
+        System.out.println("-------------------------");
+       
+        for(Integer numero: chance){
+            System.out.println(numero);
+            // otras operaciones que usan name
+        }
         
+        HashMap<String, String> glosario = new HashMap<String, String>();
+        glosario.put("perro", "mamífero perteneciente a la familia de los cánidos");
+        glosario.put("gato", "mamífero perteneciente a la familia de los félidos");
+        glosario.put("chinchilla", "mamífero perteneciente a la familia de los roedores");
+        glosario.put("alpaca", "mamífero perteneciente a la familia de los cámelidos");
+     
+        System.out.println(glosario);
 
+        System.out.println(glosario.get("chinchilla"));
 
+        /* extracción llaves */
+        Set<String> keys = glosario.keySet();
+        System.out.println(keys);
+
+        for(String key : keys){
+            System.out.println(key);
+            System.out.println(glosario.get(key));
+        }
     }
 }
