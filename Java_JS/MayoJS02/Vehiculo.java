87e70cd90d3ffcf5c8cd80364c9a8527530d0431
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-07 22:06:09 -0500

    New

--- Java_JS/MayoJS02/Vehiculo.java
+++ Java_JS/MayoJS02/Vehiculo.java
@@ -1,5 +1,92 @@
 package Java_JS.MayoJS02;
 
 public class Vehiculo {
+ 
+    private int pasajeros;
+    private int velocidad;
+    private int ruedas;
+    private int peso;
+    private int puertas;
+    private String carroceria;
+    private String categoria;
+    private boolean automatico;
+    private double cilindraje;
+
+    /* Getters and setters */
     
+    public int getPasajeros(){
+        return getPasajeros();
+    }
+
+    public void setPasajeros(int pasajeros){
+        this.pasajeros = pasajeros;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getRuedas() {
+        return ruedas;
+    }
+
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
+
+    public String getCarroceria() {
+        return carroceria;
+    }
+
+    public void setCarroceria(String carroceria) {
+        this.carroceria = carroceria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isAutomatico() {
+        return automatico;
+    }
+
+    public void setAutomatico(boolean automatico) {
+        this.automatico = automatico;
+    }
+
+    public double getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(double cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+       
+
+
 }
--- Java_JS/MayoJS02/VehiculoTest.java
+++ Java_JS/MayoJS02/VehiculoTest.java
@@ -2,4 +2,13 @@ package Java_JS.MayoJS02;
 
 public class VehiculoTest {
     
+    public static void main(String[] args) {
+        
+        /* Instancia */
+
+        Vehiculo mustang = new Vehiculo();
+            mustang.setPasajeros(4);
+            System.out.println(mustang.getPasajeros());
+
+    }
 }
