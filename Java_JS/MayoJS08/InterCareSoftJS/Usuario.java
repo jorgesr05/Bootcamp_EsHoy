1dadb2e9fa672cd954f986b296fb4f3ae27713dd
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-11 23:57:59 -0500

    New

---
+++ Java_JS/MayoJS08/InterCareSoftJS/Usuario.java
@@ -0,0 +1,36 @@
+package Java_JS.MayoJS08.InterCareSoftJS;
+
+public class Usuario {
+    
+    protected Integer id;
+    protected int pin;
+   
+    
+   
+   // Implementar un constructor que tome un ID
+    public Usuario(Integer id) {
+        this.id = id;
+    }
+
+    // PARA HACER: Getters y Setters
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public int getPin() {
+        return pin;
+    }
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+    
+    
+    
+
+    
+
+}
+
---
+++ Java_JS/MayoJS08/InterCareSoftJS/UsuarioCompatibleconHIPAA.java
@@ -0,0 +1,9 @@
+package Java_JS.MayoJS08.InterCareSoftJS;
+
+public interface UsuarioCompatibleconHIPAA {
+    abstract boolean assignPin(int pin);
+    abstract boolean accessAuthorized(Integer confirmedAuthID);
+}
+
+
+
