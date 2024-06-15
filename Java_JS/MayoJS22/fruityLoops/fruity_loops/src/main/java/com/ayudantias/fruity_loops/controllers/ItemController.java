9dfddb9e536f4c3e1daa66efea725f2b7186b1fd
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-15 18:03:21 -0500

    New

---
+++ Java_JS/MayoJS22/fruityLoops/fruity_loops/src/main/java/com/ayudantias/fruity_loops/controllers/ItemController.java
@@ -0,0 +1,33 @@
+package com.ayudantias.fruity_loops.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ayudantias.fruity_loops.models.Item;
+
+
+
+@Controller
+public class ItemController {
+
+    @GetMapping("/")
+    public String root(Model model) {
+
+        ArrayList<Item> fruits = new ArrayList<Item>();
+        fruits.add(new Item("Manzana", 2));
+        fruits.add(new Item("Platano", 2.5));
+        fruits.add(new Item("Mango", 3.2));
+        fruits.add(new Item("Fresa", 4.4));
+        fruits.add(new Item("Coco", 5.5));
+        fruits.add(new Item("Lucuma", 3.5));
+
+        model.addAttribute("fruits", fruits);
+
+        return "index.jsp";
+    }
+    
+
+}
