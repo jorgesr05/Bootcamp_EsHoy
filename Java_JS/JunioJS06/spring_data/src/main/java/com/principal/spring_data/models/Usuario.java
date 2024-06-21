986312ce61bcdf67068c5d8c2a985e05f6d4a2c9
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-21 05:18:51 -0500

    Update Usuario.java

--- Java_JS/JunioJS06/spring_data/src/main/java/com/principal/spring_data/models/Usuario.java
+++ Java_JS/JunioJS06/spring_data/src/main/java/com/principal/spring_data/models/Usuario.java
@@ -1,5 +1,25 @@
 package com.principal.spring_data.models;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor //Crea el CONSTRUCTOR sin argumentos
 public class Usuario {
+
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String telefono;
+    private Date cumpleaños;
+    private Boolean soltero;
+    private Double estatura;
     
 }
