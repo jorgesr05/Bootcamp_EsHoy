e5e075b761538b68f6d565f92357005d27108e5d
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-22 17:11:40 -0500

    New

--- Java_JS/JunioJS06/spring_data/src/main/java/com/principal/spring_data/models/Usuario.java
+++ Java_JS/JunioJS06/spring_data/src/main/java/com/principal/spring_data/models/Usuario.java
@@ -2,6 +2,23 @@ package com.principal.spring_data.models;
 
 import java.util.Date;
 
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,15 +28,63 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor //Crea el CONSTRUCTOR sin argumentos
+@Entity //Nos genera la entidad para usarlo en MySQL
+@Table(name ="usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @UniqueElements
+    @NotNull
+    @Size(min=8, max=8)
     private String dni;
+
+    @NotNull
+    @Size(min=5, max=200)
     private String nombre;
+
+    @NotNull
+    @Size(min=5, max=200)
     private String apellido;
+
+    @NotNull
+    @Email
     private String correo;
+
+    @NotNull
+    @Size(min=9, max=9)
     private String telefono;
+
+    @NotNull
+    @Past //Verifica que sea en el pasado
     private Date cumpleaños;
+
+    @NotNull
     private Boolean soltero;
+
+    @NotNull
+    @Min((long) 40.0)
+    @Max((long) 260.0)
     private Double estatura;
     
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+
+    /* CREACION Y ACTUALIZACION DE FECHAS */
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
 }
